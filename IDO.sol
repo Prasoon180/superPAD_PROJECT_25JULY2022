667ba9954f708aad614baaf414f7e811e0d101ce
Prasoon180 <101745026+Prasoon180@users.noreply.github.com>
2022-07-27 16:46:39 +0530

    Update IDO.sol

--- IDO.sol
+++ IDO.sol
@@ -1,7 +1,5 @@
 //SPDX-License-Identifier:GPL-3.0
 pragma solidity ^0.8.15;
-//import "github.com/Prasoon180/superPAD_final_project/blob/main/BEP20.sol";
-//import "github.com/Prasoon180/superPAD_final_project/blob/main/IBEP20.sol";
 import "github.com/Prasoon180/superPAD_PROJECT_25JULY2022/blob/main/IBEP20.sol";
 
 // till here BEP20 token formation done
@@ -12,8 +10,8 @@ IBEP20  public  bep20Address;
 uint public _weeksforPrivate;
 uint public _weeksforPublic;                   // define variable of weeks and months of individual item
 uint public _monthsforReward;
-uint public _monthsforReserve;
-uint public _monthsforTeam;
+uint public _monthsforReserve = 13;    // due to 1 year lockup
+uint public _monthsforTeam = 13;       // due to 1 year lockup
 uint public _monthsforMarket;
 uint public _monthsforLiquidity;
 
@@ -22,7 +20,7 @@ uint public _TGEforPublic;
 uint public _TGEforReserve;
 uint public _TGEforReward;                    // define variable of TGE of individual item
 uint public _TGEforTeam;
-uint public _TGEforLiquidity;
+uint public  _TGEforLiquidity;
 uint public _TGEforMarket;
 address public owner;
 
@@ -31,8 +29,8 @@ constructor(address _bep20Address) {
     _TGEforPrivate = block.timestamp; 
     _TGEforPublic = block.timestamp;
     _TGEforReward = block.timestamp; //  here TGE is initial time permanently set for individual item
-    _TGEforReserve = block.timestamp;   
-    _TGEforTeam = block.timestamp;
+    _TGEforReserve = block.timestamp + 31104000 seconds;   // 12 months = 31104000 seconds
+    _TGEforTeam = block.timestamp + 31104000 seconds;       // 12 months = 31104000 seconds
     _TGEforLiquidity = block.timestamp;
     _TGEforMarket = block.timestamp;  
     owner = msg.sender;              // owner of smart contract
@@ -49,13 +47,13 @@ function _private(address to , uint _amount) public onlyOwner  {
     // 1 WEEK = 604800 seconds            // function define for private
 
 
-if(_weeksforPrivate >= 0 && _weeksforPrivate < 41) {                // _weeks = 41
+if(_weeksforPrivate >= 0 && _weeksforPrivate < 40) {                // _weeks = 40
     require(block.timestamp > _TGEforPrivate, "1 WEEK NOT OVER");
      bep20Address.transfer( to, _amount);                           // 1 WEEK = 604800 seconds
      _weeksforPrivate ++;
     _TGEforPrivate += 604800 seconds;
 }
-if(_weeksforPrivate >= 41) {
+if(_weeksforPrivate >= 40) {
      require(block.timestamp + _TGEforPrivate < _TGEforPrivate, "YOUR TIME PERIOD IS OVER");
 }
 
@@ -64,13 +62,13 @@ if(_weeksforPrivate >= 41) {
 function _public(address to, uint _amount) public onlyOwner {
                  // // function define for public
                // 1 WEEK = 604800 seconds
-      if(_weeksforPublic >= 0 && _weeksforPublic < 15) {                   // _weeks = 15
+      if(_weeksforPublic >= 0 && _weeksforPublic < 14) {                   // _weeks = 15
           require(block.timestamp > _TGEforPublic, "1 WEEK NOT OVER");
           bep20Address.transfer( to, _amount);
           _weeksforPublic ++;
           _TGEforPublic += 604800 seconds;                // 1 WEEK = 604800 seconds
       }
-    if(_weeksforPublic >= 15) {
+    if(_weeksforPublic >= 14) {
      require(block.timestamp + _TGEforPublic < _TGEforPublic, "YOUR TIME PERIOD IS OVER");
      
 }  
@@ -94,22 +92,16 @@ function reward(address to, uint _amount) public {
 
 function reserve(address to, uint _amount) public onlyOwner {
                        // function define for reserve
-    if(_monthsforReserve >= 0 && _monthsforReserve < 14) {                           // _weeks = 14
-        require(block.timestamp > _TGEforReserve, "LOCKUP PERIOD NOT OCCUR PROPERLY FOR RESRVE");
-        _monthsforReserve ++;
-        _TGEforReserve += 2592000 seconds;          // 1 MONTH = 30 DAYS ;   30 DAYS = 2592000 seconds
-         }
-    if(_monthsforReserve >= 14 && _monthsforReserve < 26) {                // _weeks 14 to 26
+ 
+    if(_monthsforReserve >= 13 && _monthsforReserve < 26) {                // _weeks 13 to 25
        require(block.timestamp > _TGEforReserve, "1 MONTH NOT OVER FOR RESERVE");
-          bep20Address.transfer( to, _amount);
+          bep20Address.transfer( to, _amount);    // 1 MONTH = 30 DAYS ;   30 DAYS = 2592000 seconds
           _monthsforReserve ++;
           _TGEforReserve += 2592000 seconds;  
     }
      if(_monthsforReserve >=26) {
        require(block.timestamp + _TGEforReserve < _TGEforReserve, "RESERVE TIME PERIOD IS OVER"); 
-        
-    }
-
+        }
 }
 
 function market(address to, uint _amount) public {
@@ -128,14 +120,10 @@ function market(address to, uint _amount) public {
       
       function team(address to, uint _amount) public {
                  // function define for team
-          if(_monthsforTeam >= 0 && _monthsforTeam < 14) {                     // _weelks = 14 
-        require(block.timestamp > _TGEforTeam, "LOCKUP PERIOD NOT OCCUR PROPERLY FOR TEAM");
-        _monthsforTeam ++;
-        _TGEforTeam += 2592000 seconds;          // 1 MONTH = 30 DAYS ;   30 DAYS = 2592000 seconds
-         }
-    if(_monthsforTeam >= 14 && _monthsforTeam < 26) {                         // _weeks = 26
+
+    if(_monthsforTeam >= 13 && _monthsforTeam < 26) {                         // _weeks = 13 to 25
        require(block.timestamp > _TGEforTeam, "1 MONTH NOT OVER FOR TEAM");
-          bep20Address.transfer( to, _amount);
+          bep20Address.transfer( to, _amount);    // 1 MONTH = 30 DAYS ;   30 DAYS = 2592000 seconds
           _monthsforTeam ++;
           _TGEforTeam += 2592000 seconds;  
     }
